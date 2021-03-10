4085f501794b64789efd71b69e56b0c36795c374
Gavin Brown <gavin.brown@invisible-systems.com>
2021-03-10 10:36:11 +0000

    AWS Amplify Auth state change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Amplify, {API,graphqlOperation} from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
@@ -49,13 +52,23 @@ const deleteTodo = `mutation deleteTodo($id: ID!) {
 }`;
 
 export const App = () => {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
   const [id, setId] = useState('');
   const [todo, setTodo] = useState([]);
   const [value, setValue] = useState('');
   const [addBtn, setAddBtn] = useState(true);
   const [updateBtn, setUpdateBtn] = useState(false);
 
+  const authStateChange = () => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  };
+
   useEffect(async () => {
+    authStateChange();
     const todo = await API.graphql(graphqlOperation(readTodo));
     setTodo(todo.data.listTodo.items);
   });
@@ -115,40 +128,45 @@ export const App = () => {
   );
 
   return (
-    <div className="App">
-      <header className="App-header" alt="logo">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Todo App</h1>
-      </header>
-      <div className="container">
-        {addBtn ?
-          <form onSubmit={handleSubmit}>
-            <div className="input-group mb-3">
-              <input type="text" className="form-control form-control-lg" placeholder="new todo" />
-              <div className="input-group-append">
-                <button className="btn btn-primary" type="submit">
-                  Add todo
-                </button>
+    authState === AuthState.SignedIn && user ? (
+      <div className="App">
+        <header className="App-header" alt="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Todo App</h1>
+          <AmplifySignOut />
+        </header>
+        <div className="container">
+          {addBtn ?
+            <form onSubmit={handleSubmit}>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control form-control-lg" placeholder="new todo" />
+                <div className="input-group-append">
+                  <button className="btn btn-primary" type="submit">
+                    Add todo
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
-        : null}
-        {updateBtn ?
-          <form onSubmit={handleUpdate}>
-            <div className="input-group mb-3">
-              <input type="text" className="form-control form-control-lg" placeholder="Update todo" />
-              <div className="input-group-append">
-                <button className="btn btn-primary" type="submit" >Update todo</button>
+            </form>
+          : null}
+          {updateBtn ?
+            <form onSubmit={handleUpdate}>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control form-control-lg" placeholder="Update todo" />
+                <div className="input-group-append">
+                  <button className="btn btn-primary" type="submit" >Update todo</button>
+                </div>
               </div>
-            </div>
-          </form>
-        : null}
+            </form>
+          : null}
+        </div>
+        <div className="container">
+          {data}
+        </div>
       </div>
-      <div className="container">
-        {data}
-      </div>
-    </div>
+    ) : (
+      <AmplifyAuthenticator />
+    )
   );
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default App;
